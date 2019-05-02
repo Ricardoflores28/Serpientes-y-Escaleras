f032745ae0d0c525df630dc0595e2e61d8fc93dc
Ricardoflores28 <48325337+Ricardoflores28@users.noreply.github.com>
2019-05-01 19:52:00 -0500

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace serpintes_y_escaleras
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            txt1.Text = "";
+
+            dado b = new dado();
+            jugador a = new jugador(1);
+            jugador c = new jugador(2);
+
+            int x, y;
+
+            do
+            {
+                x = a.adelante(b.num_random());
+                y = c.adelante(b.num_random());
+
+                txt1.Text =txt1.Text + "Jugador #1: " + x.ToString() + "  <---->  Jugador #2: " + y.ToString()+ "\r\n";
+            } while (x <= 100 && y <= 100);
+
+            if (x >= 100)
+                marcador.Text = "Win Jugador 1";
+            else
+                marcador.Text = "Win Jugador 2";
+
+        }
+    }
+}
---
+++ jugador.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace serpintes_y_escaleras
+{
+    class jugador : dado
+    {
+        protected int posicion;
+        protected int num_jugador;
+        
+        //iniciamos el constructor
+        public jugador(int numero)
+        {
+            num_jugador = numero;
+            posicion = 0;
+        }
+
+        public int adelante(int random)
+        {
+            posicion = posicion + random;
+
+            if (posicion == 12) //Escalera
+                posicion += 15;
+
+            else if (posicion == 26) //Serpiente
+                posicion -= 7;
+
+            else if (posicion == 47) //Escalera
+                posicion += 16;
+
+            else if (posicion == 19) //Serpiente
+                posicion -= 11;
+
+            else if (posicion == 57) //Escalera
+                posicion += 18;
+
+            else if (posicion == 24) //Serpiente
+                posicion -= 14;
+
+            else if (posicion == 31) //Escalera
+                posicion += 20;
+
+            else if (posicion == 40) //Serpiente
+                posicion -= 30;
+
+            else if (posicion == 66) //Escalera
+                posicion += 16;
+
+            else if (posicion == 89) //Serpiente
+                posicion -= 35;
+
+            else if (posicion == 23) //Escalera
+                posicion += 70;
+
+            else if (posicion == 19) //Serpiente
+                posicion -= 9;
+
+            else if (posicion == 81) //Escalera
+                posicion += 18;
+
+            else if (posicion == 62) //Serpiente
+                posicion -= 32;
+
+            else if (posicion == 77) //Escalera
+                posicion += 34;
+
+            else if (posicion == 87) //Serpiente
+                posicion -= 98;
+
+            return posicion;
+        }
+
+    }
+}
